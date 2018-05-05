a53290709ee0d91ea0a1c19b45f3d764c226ce5c
Tomasz Sieczko <sieczko.tomasz@gmail.com>
2018-05-04 17:10:26 -0700

    Captured output from command run from program

--- geo.cpp
+++ geo.cpp
@@ -7,11 +7,19 @@ int main(int argc, char** argv)
 {
    FILE* pipe = popen("ls /", "r");
    char buffer[100]={0};
+   string response;
    //fscanf(pipe, "% s", buffer);
-   fgets(buffer, 100, pipe);
+   //fgets(buffer, 100, pipe);
+   //fgets(buffer, 100, pipe);
    //printf("%s", buffer);
+   while (fscanf(pipe, "%s", buffer) != EOF)
+   {
+      response = response + buffer + " ";
+   }
    for (int i=0; i<100; i++)
       printf("%c", buffer[i]);
+   printf("\n");
+   cout << response << endl;
    pclose(pipe);
    return 0;
 }
