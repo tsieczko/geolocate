7745ffb501accc2279109ebb0b5e42eff2aeb3b6
Tomasz Sieczko <sieczko.tomasz@gmail.com>
2018-05-07 20:58:41 -0700

    Final Version

--- cpp/geo.cpp
+++ cpp/geo.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
       {
          response = response + buffer + " ";
       }
-      cout << response << endl;
+      //cout << response << endl;
       pclose(pipe);
 
       // parse results
